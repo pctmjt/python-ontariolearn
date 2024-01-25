26006c58983e4620ca602fa079ad4d4109c369eb
pctmjt <144561401+pctmjt@users.noreply.github.com>
2024-01-25 14:17:00 -0500

    added my first file
    Hello

---
+++ hello.py
@@ -0,0 +1,13 @@
+"""
+The Hello Program - Peter Thompson
+
+This is a program that prints out "Hello World!".
+To use it, run `python hello.py`.
+"""
+
+
+def main():
+    print('Hello World')
+
+
+main()
